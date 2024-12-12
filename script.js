5fdc4b0ba9d2b502757e63867f2493ed507e8883
asmaasabet <asmaasabet28@gmail.com>
2024-12-12 05:00:17 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// var age =prompt("how old are you?");
+// var ageperhours = age *8760 ;
+// document.write(ageperhours);
+ var result =prompt("ما هي نتيجتك؟");
+ if(result>=50){
+   document.write("pass");
+ }else {
+  document.write("failed");
+ }
+//  for(var year=2024 ;year>=1919 ;year--){
+
+  // if(year==1948){
+    //  continue;
+  //  }
+  // console.log(year);
+//  }
+
+// var age =prompt("how old are you?");
+// var ageperhours = age *8760 ;
+// document.write(ageperhours + "<h1>hour</h1>");
